66c216c2dfac3d21f3537e225d83faece062416c
kasimkid <107450115+kasimkid@users.noreply.github.com>
2023-06-30 00:54:38 +0000

    prueba

--- src/app.js
+++ src/app.js
@@ -5,22 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'the keys', 'the car'];
-let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
-
-
-window.onload = function() {
-
-  dateWho = 
-  dateAction =
-  dateWhat =
-  dateWhen =
-
-
-
-
-
-console.log("Hello Rigo from the console!");
+window.onload
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+function random (who, action, what, when){
+  var randomWho = [Math.floor(Math.random() * values.length)];
+  var randomAction = [Math.floor(Math.random() * values.length)];
+  var randomWhat = [Math.floor(Math.random() * values.length)];
+  var randomWhen = [Math.floor(Math.random() * values.length)];
+  return (
+    who[randomWho] +
+    " " +
+    action[randomAction] +
+    " " +
+    what[randomWhat] +
+    " " +
+    when[randomWhen]
+  );
 };
+var homework = document.getElementById('homework');
+homework.innerHTML= random(who, action, what, when);
+window.onload = onload;
+console.log("Hello Rigo from the console!");
+// randomWho, randomAction, randomWhat, randomWhen
